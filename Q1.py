fc0f5b3fa6d9ef4257e2b4c1b6651f3d0d6a7308
clairelubash <clubash@udel.edu>
2019-10-14 17:36:28 -0400

    First commit

---
+++ Q1.py
@@ -0,0 +1,18 @@
+# Project Euler - Question 1
+
+# Find the sum of all the multiples of 3 or 5 below 1000.
+
+
+import time
+
+start = time.time()
+
+n = 1000
+ans = 0
+
+for i in range(0,n):
+    if (i%3 == 0) | (i%5 == 0):
+        ans += i
+
+print(ans)
+print('Time elapsed:', (time.time() - start), 'seconds')
